a99087c4d7628c7e99feb7db69e930e5e5417268
Ali Vidler <alisonvidler@gmail.com>
2025-03-20 15:25:42 -0400

    Changing the question and hint box size

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 const questionsAndHints = [
   { question: "Walk me through your resume", hint: "Use relevant prior experience." },
   { question: "What are your strengths?", hint: "Focus on skills that relate to the job." },
-  { question: "Describe a challenge you've faced at work.", hint: "Choose a situation that shows problem-solving." }
+  { question: "Describe a challenge you've faced at work.", hint: "Choose a situation that shows problem-solving." },
+  { question: "Tell me about a time you were delayed on a project", hint: "Use a situation where you were able to communicate expectations." },
+  { question: "What experiences have helped prepare you for this role?", hint: "Use 2 relevant experience combined for this role" },
+  { question: "How would your team describe you ", hint: "Choose a positive attribute that you might have been given as kudos" },
+  { question: "Why do you want this job?", hint: "Role, team, company mission, etc. Choose something that speaks to your passions." },
+  { question: "What is your greatest strength", hint: "Be prepared to give an example." }
 ];
 
 // for (const item of questionsAndHints) {
